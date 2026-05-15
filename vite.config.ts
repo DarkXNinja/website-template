51b78f182c521b21bef7b9317f509aa49541d41d
Aadarsh Senthil <43424645+DarkXNinja@users.noreply.github.com>
2026-05-15 20:10:54 +0000

    github pages edits

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repoName = 'website-template';
+
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   plugins: [react()],
 });
